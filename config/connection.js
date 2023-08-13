9f02af9e9ec31bc221cecef2009aa9867f7e97f3
Allison Serrano <allisonserrano171@yahoo.com>
2023-08-13 17:37:56 -0400

    connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
+//need to add jawsDB to deploy on github
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
