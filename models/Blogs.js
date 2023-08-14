f5f3f384ccb2bac9cc662dcbf8fd8772bf87b5a7
Allison Serrano <allisonserrano171@yahoo.com>
2023-08-13 21:00:14 -0400

    blogs model

---
+++ models/Blogs.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blogs extends Model {}
+
+Blogs.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogs',
+  }
+);
